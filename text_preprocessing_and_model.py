66300d3d399af39d939c63c990b0f6fa22b0010d
1Lalit7 <118678580+1Lalit7@users.noreply.github.com>
2025-01-31 20:10:48 +0530

    Update text_preprocessing_and_model.py

--- text_preprocessing_and_model.py
+++ text_preprocessing_and_model.py
@@ -101,8 +101,9 @@ class FineTunedRobertaClassifier(nn.Module):
 
 def roberta_model_based_classifier(input_text : str) -> torch.Tensor:
 
-    roberta_tokenizer = RobertaTokenizer.from_pretrained(r"models/roberta_model/roberta_tokenizer/")
-    with open(r"models\roberta_model\roberta_model_config.json", 'r') as file:
+    roberta_tokenizer = RobertaTokenizer.from_pretrained(r"models/roberta_model/roberta_tokenizer")
+    print("Roberta Tokenizer Loaded")
+    with open(r"models/roberta_model/roberta_model_config.json", 'r') as file:
         config_file = json.load(file)
     config = RobertaConfig.from_dict(config_file)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
